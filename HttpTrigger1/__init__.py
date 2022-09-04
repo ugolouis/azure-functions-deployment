42da95e0145a0e7d5910b81430f3c7931215f26e
ugolouis <louis_ul@yahoo.co.uk>
2022-09-05 00:39:46 +0100

    fifth commit

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This is my fourth commit.")
+        return func.HttpResponse(f"Hello, {name}. This is my fifth commit.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
